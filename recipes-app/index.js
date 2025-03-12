3d0b8418b66811bbc9cc9407b397797fb6a1a0af
AJIMANO2002 <ajimano2002@gmail.com>
2025-03-12 06:30:31 +0530

    mongodb update

--- recipes-app/index.js
+++ recipes-app/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/recipes", recipeRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
